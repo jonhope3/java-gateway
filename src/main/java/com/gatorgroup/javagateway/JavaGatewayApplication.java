cf5ba7c16ddbe7124374b035eab5fa9a70b553c9
Jon Hope <jon.hope@arity.com>
2019-04-18 11:12:57 -0500

    Initialize repo

---
+++ src/main/java/com/gatorgroup/javagateway/JavaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gatorgroup.javagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGatewayApplication.class, args);
+	}
+
+}
